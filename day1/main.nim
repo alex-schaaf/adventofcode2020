2edfdd74be68d589b498ca82829e0be12f681868
Alex <17277800+alex-schaaf@users.noreply.github.com>
2020-12-02 00:46:06 +0100

    nim day1.2

--- day1/main.nim
+++ day1/main.nim
@@ -8,7 +8,30 @@ for i, line in lines:
     numbers.add(line.parseInt())
 
 # exercise 1
-for num in numbers:
-    if (2020 - num in numbers):
-        echo num, " * ", 2020 - num, " = ", num * (2020 - num)
-        break
+proc solve(numbers: seq[int], sum: int): seq[int] =
+    var sol: seq[int] = @[]
+    for num in numbers:
+        if (sum - num in numbers):
+            sol.add(num)
+            sol.add(sum - num)
+            break
+    return sol
+
+let sol1 = solve(numbers, 2020)
+echo sol1, sol1[0] * sol1[1]
+
+# exercise 2
+var num2: int
+var num3: int
+var sum: int
+
+for i, num1 in numbers:
+    for j in i+1 .. numbers.len() - 1:
+        for k in j+1 .. numbers.len() - 1:
+            num2 = numbers[j]
+            num3 = numbers[k]
+            sum = num1 + num2 + num3
+            if sum == 2020:
+                let prod = num1 * num2 * num3
+                echo num1, " * ", num2," * ", num3," = ", prod
+                break
