09ee8945e9a34d7ab396cee636528151c5f781de
alex <17277800+alex-schaaf@users.noreply.github.com>
2020-12-04 11:35:31 +0100

    Go solution to day 4.1

--- README.md
+++ README.md
@@ -5,4 +5,4 @@
 1. [Go](https://github.com/alex-schaaf/adventofcode2020/blob/main/day1/main.go), [Nim](https://github.com/alex-schaaf/adventofcode2020/blob/main/day1/main.nim)
 2. [Go](https://github.com/alex-schaaf/adventofcode2020/blob/main/day2/main.go), [Python (Recursive)](https://github.com/alex-schaaf/adventofcode2020/blob/main/day2/python/main.py)
 3. [Go](https://github.com/alex-schaaf/adventofcode2020/blob/main/day3/main.go)
-4. [Python](https://github.com/alex-schaaf/adventofcode2020/blob/main/day4/main.py)
+4. [Go](https://github.com/alex-schaaf/adventofcode2020/blob/main/day4/main.go), [Python](https://github.com/alex-schaaf/adventofcode2020/blob/main/day4/main.py) 
--- day4/main.go
+++ day4/main.go
@@ -7,6 +7,9 @@ import (
 	"strings"
 )
 
+var fields = []string{"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid"}
+var requiredFields = []string{"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
+
 func readPassportStrings(filepath string) []string {
 	content, err := ioutil.ReadFile(filepath)
 	if err != nil {
@@ -31,6 +34,20 @@ func parsePassportStrings(passportStrings []string) []map[string]string {
 	return passports
 }
 
+func exercise1(passports []map[string]string) int {
+	var validPasswords = 0
+	for _, passport := range passports {
+		var isValid = 1
+		for _, field := range requiredFields {
+			if _, ok := passport[field]; !ok {
+				isValid = 0
+			}
+		}
+		validPasswords += isValid
+	}
+	return validPasswords
+}
+
 func main() {
 	passportStrings := readPassportStrings("./test")
 	passports := parsePassportStrings(passportStrings)
@@ -39,4 +56,6 @@ func main() {
 		fmt.Println(passport)
 	}
 
+	sol1 := exercise1(passports)
+	fmt.Printf("Exercise 1: %d valid passports detected.\n", sol1)
 }
