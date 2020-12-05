aa7c91ad83b41051ea578cadb4e4af1e971abd2e
alex <17277800+alex-schaaf@users.noreply.github.com>
2020-12-05 10:05:18 +0100

    Golang solution to day 5.2

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 2. [Go](https://github.com/alex-schaaf/adventofcode2020/blob/main/day2/main.go), [Python (Recursive)](https://github.com/alex-schaaf/adventofcode2020/blob/main/day2/python/main.py)
 3. [Go](https://github.com/alex-schaaf/adventofcode2020/blob/main/day3/main.go)
 4. [Go](https://github.com/alex-schaaf/adventofcode2020/blob/main/day4/main.go), [Python](https://github.com/alex-schaaf/adventofcode2020/blob/main/day4/main.py) 
+5. [Go](https://github.com/alex-schaaf/adventofcode2020/blob/main/day4/main.go)
--- day5/main.go
+++ day5/main.go
@@ -23,9 +23,7 @@ func readFileLines(filepath string) []string {
 }
 
 func binaryPartition(pass string, low int, high int, lower byte, higher byte) int {
-
 	char := pass[0]
-	// fmt.Println(char, low, high)
 	if char == lower {
 		high = (high-low)/2 + low
 	} else {
@@ -43,31 +41,51 @@ func binaryPartition(pass string, low int, high int, lower byte, higher byte) in
 	return binaryPartition(pass[1:], low, high, lower, higher)
 }
 
+func getSeatID(row int, col int) int {
+	return row*8 + col
+}
+
 func main() {
 	boardingPasses := readFileLines("./input")
-	// fmt.Println("L:", []rune("L")[0], "R:", []rune("R")[0])
 	var seatIDs []int
 	var maxID int
 
 	for _, pass := range boardingPasses {
 		rowCode := pass[:len(pass)-3]
 		colCode := pass[len(pass)-3:]
-		// fmt.Println(rowCode, colCode)
 
 		row := binaryPartition(rowCode, 0, 127, lowerRow, upperRow)
 		col := binaryPartition(colCode, 0, 7, lowerCol, upperCol)
 
-		// fmt.Println("row:", row)
-		// fmt.Println("col:", col)
-
-		seatID := row*8 + col
+		seatID := getSeatID(row, col)
 		if seatID > maxID {
 			maxID = seatID
 		}
 
-		// fmt.Println("seadID:", seatID)
 		seatIDs = append(seatIDs, seatID)
 
 	}
+
+	var seats [128][8]bool
+
+	for r := 0; r <= 127; r++ {
+		for c := 0; c <= 7; c++ {
+			for _, seatID := range seatIDs {
+				if seatID == getSeatID(r, c) {
+					seats[r][c] = true
+				}
+			}
+		}
+	}
+	var mySeatID int
+	for r := 8; r < 117; r++ {
+		for c := 0; c <= 7; c++ {
+			if !seats[r][c] {
+				mySeatID = getSeatID(r, c)
+			}
+		}
+	}
+
 	fmt.Printf("Exercise 1: The highest seat ID is %d.\n", maxID)
+	fmt.Printf("Exercise 2: My seat ID is %d.\n", mySeatID)
 }
