3b0d3abd439d5411c9f23364248e63e472e1f1b0
alex <17277800+alex-schaaf@users.noreply.github.com>
2020-12-06 08:23:35 +0100

    Golang solution to day 6.2

--- day6/main.go
+++ day6/main.go
@@ -15,15 +15,30 @@ func main() {
 	groups := readFileLines("./input", "\n\n")
 
 	var groupCountSum int
+	var groupCountSum2 int
 	for _, group := range groups {
 		grp := strings.ReplaceAll(group, "\n", "")
-		fmt.Println(grp)
 		charMap := make(map[rune]int)
 		for _, char := range grp {
 			charMap[char]++
 		}
 		groupCountSum += len(charMap)
-		fmt.Println("---")
+
+		// exercise 2
+		groupLen := 1
+		for _, char := range group {
+			if char == rune('\n') {
+				groupLen++
+			}
+		}
+
+		for _, count := range charMap {
+			if count == groupLen {
+				groupCountSum2++
+			}
+		}
+
 	}
 	fmt.Printf("Exercise 1: %d questions answered with 'yes'.\n", groupCountSum)
+	fmt.Printf("Exercise 2: %d questions answered collectively with 'yes' within groups.\n", groupCountSum2)
 }
