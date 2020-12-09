6b89cfd1e27c35570b5c9e19145edff2d5ae8a19
Alex <17277800+alex-schaaf@users.noreply.github.com>
2020-12-09 23:20:04 +0100

    template script

---
+++ template.py
@@ -0,0 +1,24 @@
+import os
+from datetime import datetime
+
+template = """package main
+
+func readFileLines(filepath string, split string) []string {
+	content, _ := ioutil.ReadFile(filepath)
+	return strings.Split(string(content), split)
+}
+
+func main() {
+    lines := readFileLines("./test", "\\n")
+}
+"""
+
+day = datetime.now().strftime("%d")
+folder = f"day{day}"
+os.mkdir(folder)
+
+with open(folder + "/main.go", "w") as file:
+    file.write(template)
+
+open(folder + "/test", "w").close()
+open(folder + "/input", "w").close()
