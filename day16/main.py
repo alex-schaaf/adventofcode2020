caa3b0845a34ea613a3d9cb4864283c89fdd1228
Alex <17277800+alex-schaaf@users.noreply.github.com>
2020-12-16 09:12:52 +0100

    Python solution to 16.1

--- day16/main.py
+++ day16/main.py
@@ -27,8 +27,31 @@ def parse_input(filepath: str) -> Tuple:
     return rules_parsed, my_ticket, tickets_parsed
 
 
+def check_or(ranges: List[Tuple[int, int]], value: int) -> bool:
+    p1, p2 = False, False
+    (r1, r2), (r3, r4) = ranges[0], ranges[1]
+    if r1 <= value <= r2:
+        p1 = True
+    if r3 <= value <= r4:
+        p2 = True
+    return p1 or p2
+
+
 if __name__ == "__main__":
-    rules, my_ticket, tickets = parse_input("./test")
-    print(rules)
-    print(my_ticket)
-    print(tickets)
+    rules, my_ticket, tickets = parse_input("./input")
+    # print(rules)
+    # print(my_ticket)
+    # print(tickets)
+    invalid_sum = 0
+    for ticket in tickets:
+        valid_values = []
+        for value in ticket:
+            passed_any = 0
+            for rule, ranges in rules.items():
+                passed_any += check_or(ranges, value)
+            valid_values.append(bool(passed_any))
+            if not bool(passed_any):
+                invalid_sum += value
+        # print(ticket)
+        # print(valid_values)
+    print(invalid_sum)
