9527754fbb7707d0c262bf4c3b9be58a6b603790
Alex <17277800+alex-schaaf@users.noreply.github.com>
2020-12-17 10:23:02 +0100

    Python solution to 17.2

--- day17/main.py
+++ day17/main.py
@@ -7,17 +7,18 @@ def parse_input(filepath: str) -> list:
     return content.split("\n")
 
 
-def count_neighbors(y: int, x: int, z: int, world: np.ndarray) -> int:
+def count_neighbors(y: int, x: int, z: int, w: int, world: np.ndarray) -> int:
     neighbors = 0
     for dy in range(-1, 2):
         for dx in range(-1, 2):
             for dz in range(-1, 2):
-                if dy == 0 and dx == 0 and dz == 0:
-                    continue
-                try:
-                    neighbors += world[y + dy, x + dx, z + dz]
-                except IndexError:
-                    continue
+                for dw in range(-1, 2):
+                    if dy == 0 and dx == 0 and dz == 0 and dw == 0:
+                        continue
+                    try:
+                        neighbors += world[y + dy, x + dx, z + dz, w + dw]
+                    except IndexError:
+                        continue
     return neighbors
 
 
@@ -28,7 +29,7 @@ if __name__ == "__main__":
     nx = len(lines[0])
 
     # initialize world
-    world = np.zeros((ny, nx, 1))
+    world = np.zeros((ny, nx, 1, 1))
     for y, line in enumerate(lines):
         for x, char in enumerate(line):
             if char == "#":
@@ -38,23 +39,24 @@ if __name__ == "__main__":
     for cycle in range(6):
         world = np.pad(world, 1, "constant", constant_values=0)
 
-        ny, nx, nz = world.shape
+        ny, nx, nz, nw = world.shape
 
         new_world = np.copy(world)
 
         for y in range(ny):
             for x in range(nx):
                 for z in range(nz):
-                    active = bool(world[y, x, z])
-                    n = count_neighbors(y, x, z, world)
-                    if active:
-                        if 2 <= n <= 3:
-                            continue
+                    for w in range(nw):
+                        active = bool(world[y, x, z, w])
+                        n = count_neighbors(y, x, z, w, world)
+                        if active:
+                            if 2 <= n <= 3:
+                                continue
+                            else:
+                                new_world[y, x, z, w] = 0
                         else:
-                            new_world[y, x, z] = 0
-                    else:
-                        if n == 3:
-                            new_world[y, x, z] = 1
+                            if n == 3:
+                                new_world[y, x, z, w] = 1
         world = new_world
 
     print(np.count_nonzero(world))
